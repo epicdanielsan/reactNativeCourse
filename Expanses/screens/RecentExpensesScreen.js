5dacf73ef3fa5dc748c3420e679fe673e8b6d7eb
Daniel <daniel@epicquestti.com.br>
2022-11-17 07:14:03 -0300

    17-11-2022 07:13 Finished Expenses App

--- Expanses/screens/RecentExpensesScreen.js
+++ Expanses/screens/RecentExpensesScreen.js
@@ -1,21 +1,45 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { View } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
+import ErrorOverlay from "../UI/ErrorOverlay";
+import LoadingOverlay from "../UI/LoadingOverlay";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
 
 const RecentExpensesScreen = () => {
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
+
   const expensesCtx = useContext(ExpensesContext);
 
   useEffect(() => {
     const getExpenses = async () => {
-      const expenses = await fetchExpenses();
+      setIsFetching(true);
 
-      expensesCtx.setExpenses(expenses);
+      try {
+        const expenses = await fetchExpenses();
+        expensesCtx.setExpenses(expenses);
+      } catch (error) {
+        setError("Could not fetch expenses");
+      }
+
+      setIsFetching(false);
     };
     getExpenses();
   }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
+
+  if (error && !isFetching) {
+    return <ErrorOverlay message={error} onConfirm={errorHandler} />;
+  }
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
